751c5d8abe2a74d5d05d05663c80df58302bc9cb
obibaadoma <akrasikonadu@yahoo.com>
2024-01-13 18:47:17 +0000

    fixed deployment changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,25 +2,7 @@
 # exit on error
 set -o errexit
 
-# Change directory to the location of your script
-cd "$(dirname "$0")" || exit
-
-# Ensure execute permissions for the script
-chmod +x ./bin/render-build.sh
-chmod +x ./bin/rails
-
-# Install bundle if not already installed
-if ! command -v bundle &> /dev/null; then
-    gem install bundler
-fi
-
-# Install dependencies using bundle with deployment option
-bundle install --deployment
-
-# Ensure execute permissions for the rails binary
-chmod +x ./bin/rails
-
-# Run the necessary commands
+bundle install
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 bundle exec rake db:migrate
--- readme.md
+++ readme.md
@@ -61,8 +61,8 @@
 
 ## 🚀 Live Demo <a name="live-demo"></a>..
 
-- [Link to Live demo](https://www.loom.com/share/b6bc3659c096497d8033110ade38c3f6?sid=544571df-58df-4030-a0f0-b49960266c29)
-- [Quick Overview](https://obibaadoma.github.io/Expense-App/)
+- [Link to Live demo](https://expense-app-i6s3.onrender.com/)
+- [Quick Overview](https://www.loom.com/share/b6bc3659c096497d8033110ade38c3f6?sid=544571df-58df-4030-a0f0-b49960266c29)
 
 <p align="right">(<a href="#readme-top">back to top</a>)</p>
 
