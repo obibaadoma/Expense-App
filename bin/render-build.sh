3344e017175a6b66900145f3f88a0ccf7776cd1e
obibaadoma <akrasikonadu@yahoo.com>
2024-01-13 16:57:36 +0000

    fixed render.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,25 @@
 # exit on error
 set -o errexit
 
-bundle install
+# Change directory to the location of your script
+cd "$(dirname "$0")" || exit
+
+# Ensure execute permissions for the script
+chmod +x ./bin/render-build.sh
+chmod +x ./bin/rails
+
+# Install bundle if not already installed
+if ! command -v bundle &> /dev/null; then
+    gem install bundler
+fi
+
+# Install dependencies using bundle with deployment option
+bundle install --deployment
+
+# Ensure execute permissions for the rails binary
+chmod +x ./bin/rails
+
+# Run the necessary commands
 ./bin/rails assets:precompile
 ./bin/rails assets:clean
 bundle exec rake db:migrate
